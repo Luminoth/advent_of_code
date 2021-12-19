de8dbd1bbf46831c851bc7eb380d53b2fe19a255
Shane <ignignokterr@gmail.com>
2021-12-18 17:19:20 -0800

    More futzing with this

--- 2021/day18/src/main.rs
+++ 2021/day18/src/main.rs
@@ -14,6 +14,13 @@ impl SnailfishNumberType {
         }
     }
 
+    fn explode(&mut self, explosion: ExplodeType) {
+        match self {
+            Self::Number(number) => *number += explosion.value(),
+            Self::Pair(pair) => pair.propagate_explosion(explosion),
+        }
+    }
+
     fn split(&self) -> [SnailfishNumberType; 2] {
         match self {
             SnailfishNumberType::Number(number) => [
@@ -41,6 +48,30 @@ impl<T: AsRef<str>> From<T> for SnailfishNumberType {
 }
 
 #[derive(Debug)]
+enum ExplodeType {
+    Left(isize),
+    Right(isize),
+    None,
+}
+
+impl ExplodeType {
+    fn value(&self) -> isize {
+        match self {
+            Self::Left(value) => *value,
+            Self::Right(value) => *value,
+            _ => panic!("invalid explosion"),
+        }
+    }
+}
+
+#[derive(Debug)]
+enum ReduceAction {
+    None,
+    Explode(ExplodeType),
+    Split,
+}
+
+#[derive(Debug)]
 struct SnailfishNumber {
     number: [SnailfishNumberType; 2],
 }
@@ -48,6 +79,25 @@ struct SnailfishNumber {
 impl SnailfishNumber {
     fn magnitude(&self) -> isize {
         3 * self.number[0].magnitude() + 2 * self.number[1].magnitude()
+    }
+
+    fn check_explode(&self, depth: usize) -> Option<(isize, isize)> {
+        if depth >= 4 {
+            if let SnailfishNumberType::Number(left) = self.number[0] {
+                if let SnailfishNumberType::Number(right) = self.number[1] {
+                    return Some((left, right));
+                }
+            }
+        }
+        None
+    }
+
+    fn propagate_explosion(&mut self, explosion: ExplodeType) {
+        match explosion {
+            ExplodeType::Left(_) => self.number[1].explode(explosion),
+            ExplodeType::Right(_) => self.number[0].explode(explosion),
+            _ => panic!("invalid explosion propagation"),
+        }
     }
 
     fn add(self, rhs: SnailfishNumber) -> Self {
@@ -59,38 +109,73 @@ impl SnailfishNumber {
         }
     }
 
-    fn reduce(&mut self, depth: usize) -> bool {
-        match &self.number[0] {
-            SnailfishNumberType::Number(number) => {
-                if *number >= 10 {
-                    let number = self.number[0].split();
-                    self.number[0] = SnailfishNumberType::Pair(Box::new(Self { number }));
-                    return true;
-                }
-            }
-            SnailfishNumberType::Pair(pair) => {
-                if depth >= 4 {
-                    // TODO: explode... but how?
-                }
+    // TODO: this is still not right
+    fn reduce(&mut self, depth: usize) -> ReduceAction {
+        // explode left?
+        if let SnailfishNumberType::Pair(pair) = &self.number[0] {
+            if let Some((left, right)) = pair.check_explode(depth + 1) {
+                self.number[1].explode(ExplodeType::Right(right));
+
+                let ret = ReduceAction::Explode(ExplodeType::Left(left));
+                self.number[0] = SnailfishNumberType::Number(0);
+                return ret;
             }
         }
 
-        match &self.number[1] {
-            SnailfishNumberType::Number(number) => {
-                if *number >= 10 {
-                    let number = self.number[1].split();
-                    self.number[1] = SnailfishNumberType::Pair(Box::new(Self { number }));
-                    return true;
-                }
-            }
-            SnailfishNumberType::Pair(pair) => {
-                if depth >= 4 {
-                    // TODO: explode... but how?
-                }
+        // explode right?
+        if let SnailfishNumberType::Pair(pair) = &self.number[1] {
+            if let Some((left, right)) = pair.check_explode(depth + 1) {
+                self.number[0].explode(ExplodeType::Left(left));
+
+                let ret = ReduceAction::Explode(ExplodeType::Right(right));
+                self.number[1] = SnailfishNumberType::Number(0);
+                return ret;
             }
         }
 
-        false
+        // reduce left
+        if let SnailfishNumberType::Pair(pair) = &mut self.number[0] {
+            match pair.reduce(depth + 1) {
+                ReduceAction::Split => return ReduceAction::Split,
+                ReduceAction::Explode(explosion) => {
+                    self.number[1].explode(explosion);
+                    return ReduceAction::Explode(ExplodeType::None);
+                }
+                ReduceAction::None => (),
+            }
+        }
+
+        // reduce right
+        if let SnailfishNumberType::Pair(pair) = &mut self.number[1] {
+            match pair.reduce(depth + 1) {
+                ReduceAction::Split => return ReduceAction::Split,
+                ReduceAction::Explode(explosion) => {
+                    self.number[0].explode(explosion);
+                    return ReduceAction::Explode(ExplodeType::None);
+                }
+                ReduceAction::None => (),
+            }
+        }
+
+        // split left
+        if let SnailfishNumberType::Number(number) = &self.number[0] {
+            if *number >= 10 {
+                let number = self.number[0].split();
+                self.number[0] = SnailfishNumberType::Pair(Box::new(Self { number }));
+                return ReduceAction::Split;
+            }
+        }
+
+        // split right
+        if let SnailfishNumberType::Number(number) = &self.number[1] {
+            if *number >= 10 {
+                let number = self.number[1].split();
+                self.number[0] = SnailfishNumberType::Pair(Box::new(Self { number }));
+                return ReduceAction::Split;
+            }
+        }
+
+        ReduceAction::None
     }
 }
 
@@ -128,9 +213,10 @@ fn part1(mut numbers: VecDeque<SnailfishNumber>) {
         sum = sum.add(number);
 
         loop {
-            if !sum.reduce(0) {
+            if matches!(sum.reduce(0), ReduceAction::None) {
                 break;
             }
+            println!("reduced");
         }
         println!("step: {:?}", sum);
     }
