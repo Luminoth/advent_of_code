a451e16fe8274bb2baf10f268f3145ef7fb1a02c
Shane Lillie <ignignokterr@gmail.com>
2022-12-24 16:21:35 -0800

    idk how we're supposed to allow going backwards

--- 2022/day24/src/main.rs
+++ 2022/day24/src/main.rs
@@ -10,6 +10,10 @@ impl Position {
     fn new(x: usize, y: usize) -> Self {
         Self { x, y }
     }
+
+    fn distance(&self, other: Position) -> usize {
+        ((other.x as i64 - self.x as i64).abs() + (other.y as i64 - self.y as i64).abs()) as usize
+    }
 }
 
 #[derive(Debug)]
@@ -20,6 +24,90 @@ struct Expedition {
 impl Expedition {
     fn new(position: Position) -> Self {
         Self { position }
+    }
+
+    fn simulate(&self, map: &Map, minutes: usize, max_time: usize) -> usize {
+        assert!(map.is_cell_safe(self.position, minutes));
+
+        // if we've gone for too long, we failed
+        if minutes >= max_time {
+            //println!("max time {}", max_time);
+            return usize::MAX;
+        }
+
+        // if we're at the start we can only move down
+        if self.position.y == 0 {
+            let position = Position::new(self.position.x, self.position.y + 1);
+            assert!(map.is_cell_safe(position, minutes + 1));
+            return Expedition::new(position).simulate(map, minutes + 1, max_time);
+        }
+
+        let end = map.get_end_position();
+
+        // if we're at the end, we're done
+        if self.position == end {
+            println!("exit at {}", minutes);
+            return minutes;
+        }
+
+        let mut min_time = usize::MAX;
+
+        let current_distance = self.position.distance(end);
+
+        println!(
+            "minute {}, distance = {} from {:?} to {:?}",
+            minutes, current_distance, self.position, end
+        );
+
+        // north
+        let position = Position::new(self.position.x, self.position.y - 1);
+        let distance = position.distance(end);
+        if distance < current_distance && map.is_cell_safe(position, minutes + 1) {
+            //println!("test north {} vs {}", current_distance, distance);
+            min_time = Expedition::new(position)
+                .simulate(map, minutes + 1, max_time)
+                .min(min_time);
+        }
+
+        // south
+        let position = Position::new(self.position.x, self.position.y + 1);
+        let distance = position.distance(end);
+        if distance < current_distance && map.is_cell_safe(position, minutes + 1) {
+            //println!("test south {} vs {}", current_distance, distance);
+            min_time = Expedition::new(position)
+                .simulate(map, minutes + 1, max_time)
+                .min(min_time);
+        }
+
+        // east
+        let position = Position::new(self.position.x + 1, self.position.y);
+        let distance = position.distance(end);
+        if distance < current_distance && map.is_cell_safe(position, minutes + 1) {
+            //println!("test east {} vs {}", current_distance, distance);
+            min_time = Expedition::new(position)
+                .simulate(map, minutes + 1, max_time)
+                .min(min_time);
+        }
+
+        // west
+        let position = Position::new(self.position.x - 1, self.position.y);
+        let distance = position.distance(end);
+        if distance < current_distance && map.is_cell_safe(position, minutes + 1) {
+            //println!("test west {} vs {}", current_distance, distance);
+            min_time = Expedition::new(position)
+                .simulate(map, minutes + 1, max_time)
+                .min(min_time);
+        }
+
+        // wait
+        if map.is_cell_safe(self.position, minutes + 1) {
+            //println!("test wait");
+            min_time = self
+                .simulate(map, minutes + 1, max_time.min(min_time))
+                .min(min_time);
+        }
+
+        min_time
     }
 }
 
@@ -90,13 +178,14 @@ impl Blizzard {
                 Position::new(self.position.x, y)
             }
             Direction::East => {
-                let x =
-                    1 + (self.position.x as i64 - 1 + minutes as i64) % (map.width() - 2) as i64;
-                Position::new(x as usize, self.position.y)
+                let x = 1 + (self.position.x - 1 + minutes) % (map.width() - 2);
+                Position::new(x, self.position.y)
             }
             Direction::West => {
-                let x = 1 + (self.position.x - 1 - minutes) % (map.width() - 2);
-                Position::new(x, self.position.y)
+                assert!(self.position.x > 0);
+                let x =
+                    1 + (self.position.x as i64 - 1 - minutes as i64) % (map.width() - 2) as i64;
+                Position::new(x as usize, self.position.y)
             }
         }
     }
@@ -153,15 +242,14 @@ impl Cell {
 #[derive(Debug)]
 struct Map {
     cells: Vec<Vec<Cell>>,
-}
-
-impl From<Vec<Vec<Cell>>> for Map {
-    fn from(cells: Vec<Vec<Cell>>) -> Self {
-        Self { cells }
-    }
+    blizzards: Vec<Blizzard>,
 }
 
 impl Map {
+    fn new(cells: Vec<Vec<Cell>>, blizzards: Vec<Blizzard>) -> Self {
+        Self { cells, blizzards }
+    }
+
     #[inline]
     fn width(&self) -> usize {
         self.cells[0].len()
@@ -170,6 +258,25 @@ impl Map {
     #[inline]
     fn height(&self) -> usize {
         self.cells.len()
+    }
+
+    #[inline]
+    fn get_cell(&self, position: Position) -> &Cell {
+        &self.cells[position.y][position.x]
+    }
+
+    fn is_cell_safe(&self, position: Position, minutes: usize) -> bool {
+        if !self.get_cell(position).is_open() {
+            return false;
+        }
+
+        for blizzard in &self.blizzards {
+            if blizzard.get_position_at(self, minutes) == position {
+                return false;
+            }
+        }
+
+        true
     }
 
     fn get_start_position(&self) -> Position {
@@ -181,14 +288,24 @@ impl Map {
         unreachable!()
     }
 
-    fn render(&self, blizzards: impl AsRef<[Blizzard]>, expedition: &Expedition, minutes: usize) {
+    fn get_end_position(&self) -> Position {
+        for (x, cell) in self.cells[self.cells.len() - 1].iter().enumerate() {
+            if cell.is_open() {
+                return Position::new(x, self.cells.len() - 1);
+            }
+        }
+        unreachable!()
+    }
+
+    #[cfg(feature = "debugvis")]
+    fn render(&self, expedition: &Expedition, minutes: usize) {
         for (y, row) in self.cells.iter().enumerate() {
             for (x, cell) in row.iter().enumerate() {
                 let position = Position::new(x, y);
 
                 let mut blizzard_count = 0;
                 let mut last_direction = None;
-                for blizzard in blizzards.as_ref() {
+                for blizzard in &self.blizzards {
                     let blizzard_position = blizzard.get_position_at(self, minutes);
                     if blizzard_position == position {
                         blizzard_count += 1;
@@ -217,28 +334,14 @@ impl Map {
     }
 }
 
-fn part1(map: &Map, blizzards: impl AsRef<[Blizzard]>) {
+fn part1(map: &Map) {
     let start = map.get_start_position();
     let expedition = Expedition::new(start);
 
-    let mut minutes = 0;
-
-    println!("== Initial State == ");
-    map.render(&blizzards, &expedition, minutes);
-
-    loop {
-        minutes += 1;
-
-        println!();
-        println!("== After Mintute {} == ", minutes);
-        map.render(&blizzards, &expedition, minutes);
-
-        if minutes >= 10 {
-            break;
-        }
-    }
-
-    println!("Total minutes: {}", minutes + 1);
+    let total = expedition.simulate(map, 0, usize::MAX);
+    println!("done: {}", total);
+    //assert!(total == ???);
+    println!("Total minutes: {}", total + 1);
 }
 
 fn main() {
@@ -273,6 +376,6 @@ fn main() {
         })
         .collect::<Vec<_>>();
 
-    let map = values.into();
-    part1(&map, blizzards);
+    let map = Map::new(values, blizzards);
+    part1(&map);
 }
