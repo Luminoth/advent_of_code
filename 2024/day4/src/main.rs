50a7ff240bc6e2de0e765915f848e1adae1efdbe
Shane Lillie <ignignokterr@gmail.com>
2024-12-04 22:18:16 -0800

    Hacky part 2

--- 2024/day4/src/main.rs
+++ 2024/day4/src/main.rs
@@ -99,92 +99,90 @@ fn part1(puzzle: &[Vec<char>]) {
         y += 1;
     }
 
-    //assert!(count == 2370);
+    assert!(count == 2370);
     println!("Count: {}", count);
 }
 
-fn part1(puzzle: &[Vec<char>]) {
+fn part2(puzzle: &[Vec<char>]) {
     let mut count = 0;
 
     let mut y = 0;
-    while y < puzzle.len() {
+    while y < puzzle.len() as isize {
         let mut x = 0;
-        while x < puzzle[y].len() {
-            if puzzle[y][x] == 'X' {
-                // W
-                if x > 0 && puzzle[y][x - 1] == 'M' {
-                    if x > 1 && puzzle[y][x - 2] == 'A' {
-                        if x > 2 && puzzle[y][x - 3] == 'S' {
-                            count += 1;
+        while x < puzzle[y as usize].len() as isize {
+            if matches(puzzle, y, x, 'A') {
+                // NW / SE
+                if matches(puzzle, y - 1, x - 1, 'M') {
+                    if matches(puzzle, y + 1, x + 1, 'S') {
+                        // SW / NE
+                        if matches(puzzle, y + 1, x - 1, 'M') {
+                            if matches(puzzle, y - 1, x + 1, 'S') {
+                                count += 1;
+                            }
+                        }
+
+                        // NE / SW
+                        if matches(puzzle, y - 1, x + 1, 'M') {
+                            if matches(puzzle, y + 1, x - 1, 'S') {
+                                count += 1;
+                            }
                         }
                     }
                 }
 
-                // E
-                if x < puzzle[y].len() - 1 && puzzle[y][x + 1] == 'M' {
-                    if x < puzzle[y].len() - 2 && puzzle[y][x + 2] == 'A' {
-                        if x < puzzle[y].len() - 3 && puzzle[y][x + 3] == 'S' {
-                            count += 1;
+                // SW / NE
+                if matches(puzzle, y + 1, x - 1, 'M') {
+                    if matches(puzzle, y - 1, x + 1, 'S') {
+                        // NW / SE
+                        if matches(puzzle, y - 1, x - 1, 'M') {
+                            if matches(puzzle, y + 1, x + 1, 'S') {
+                                count += 1;
+                            }
+                        }
+
+                        // SE / NW
+                        if matches(puzzle, y + 1, x + 1, 'M') {
+                            if matches(puzzle, y - 1, x - 1, 'S') {
+                                count += 1;
+                            }
                         }
                     }
                 }
 
-                // N
-                if y > 0 && puzzle[y - 1][x] == 'M' {
-                    if y > 1 && puzzle[y - 2][x] == 'A' {
-                        if y > 2 && puzzle[y - 3][x] == 'S' {
-                            count += 1;
+                // NE / SW
+                if matches(puzzle, y - 1, x + 1, 'M') {
+                    if matches(puzzle, y + 1, x - 1, 'S') {
+                        // NW / SE
+                        if matches(puzzle, y - 1, x - 1, 'M') {
+                            if matches(puzzle, y + 1, x + 1, 'S') {
+                                count += 1;
+                            }
+                        }
+
+                        // SE / NW
+                        if matches(puzzle, y + 1, x + 1, 'M') {
+                            if matches(puzzle, y - 1, x - 1, 'S') {
+                                count += 1;
+                            }
                         }
                     }
                 }
 
-                // S
-                if y < puzzle.len() - 1 && puzzle[y + 1][x] == 'M' {
-                    if y < puzzle.len() - 2 && puzzle[y + 2][x] == 'A' {
-                        if y < puzzle.len() - 3 && puzzle[y + 3][x] == 'S' {
-                            count += 1;
+                // SE / NW
+                if matches(puzzle, y + 1, x + 1, 'M') {
+                    if matches(puzzle, y - 1, x - 1, 'S') {
+                        // SW / NE
+                        if matches(puzzle, y + 1, x - 1, 'M') {
+                            if matches(puzzle, y - 1, x + 1, 'S') {
+                                count += 1;
+                            }
                         }
-                    }
-                }
 
-                // NW
-                if y > 0 && x > 0 && puzzle[y - 1][x - 1] == 'M' {
-                    if y > 1 && x > 1 && puzzle[y - 2][x - 2] == 'A' {
-                        if y > 2 && x > 2 && puzzle[y - 3][x - 3] == 'S' {
-                            count += 1;
-                        }
-                    }
-                }
-
-                // SW
-                if y < puzzle.len() - 1 && x > 0 && puzzle[y + 1][x - 1] == 'M' {
-                    if y < puzzle.len() - 2 && x > 1 && puzzle[y + 2][x - 2] == 'A' {
-                        if y < puzzle.len() - 3 && x > 2 && puzzle[y + 3][x - 3] == 'S' {
-                            count += 1;
-                        }
-                    }
-                }
-
-                // NE
-                if y > 0 && x < puzzle[y].len() - 1 && puzzle[y - 1][x + 1] == 'M' {
-                    if y > 1 && x < puzzle[y].len() - 2 && puzzle[y - 2][x + 2] == 'A' {
-                        if y > 2 && x < puzzle[y].len() - 3 && puzzle[y - 3][x + 3] == 'S' {
-                            count += 1;
-                        }
-                    }
-                }
-
-                // SE
-                if y < puzzle.len() - 1 && x < puzzle[y].len() - 1 && puzzle[y + 1][x + 1] == 'M' {
-                    if y < puzzle.len() - 2
-                        && x < puzzle[y].len() - 2
-                        && puzzle[y + 2][x + 2] == 'A'
-                    {
-                        if y < puzzle.len() - 3
-                            && x < puzzle[y].len() - 3
-                            && puzzle[y + 3][x + 3] == 'S'
-                        {
-                            count += 1;
+                        // NE / SW
+                        if matches(puzzle, y - 1, x + 1, 'M') {
+                            if matches(puzzle, y + 1, x - 1, 'S') {
+                                count += 1;
+                            }
                         }
                     }
                 }
@@ -194,7 +192,11 @@ fn part1(puzzle: &[Vec<char>]) {
         y += 1;
     }
 
-    assert!(count == 2370);
+    // this is dumb but it's late lol
+    // (we're double counting everything)
+    count /= 2;
+
+    assert!(count == 1908);
     println!("Count: {}", count);
 }
 
@@ -207,4 +209,5 @@ fn main() {
         .collect::<Vec<_>>();
 
     part1(&puzzle);
+    part2(&puzzle);
 }
