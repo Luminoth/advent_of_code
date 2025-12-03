9fa29e97b6b75087057fa5c10b7580bc7b4da160
Shane Lillie <ignignokterr@gmail.com>
2025-12-03 15:43:21 -0800

    Clean up and generalize

--- 2025/day3/src/main.rs
+++ 2025/day3/src/main.rs
@@ -1,78 +1,12 @@
-/*
-another interesting approach to this might be to find the largest number from the right,
-starting at len - # of batteries remaining and then slice at that point and repeat until all the numbers are found
-
-// returns the index in the slice of the largest value at least remaining from the end
-fn find_largest(slice: ..., remaining: usize) -> usize {
-    let mut max = 0;
-    let mut max_idx = 0;
-    for idx in (0..slice.len()).rev().skip(remaining - 1) {
-        if slice[idx} > max {
-            max = slice[idx];
-            max_idx = idx;
-        }
-    }
-    max_idx
-}
-
-fn solve(v: ..., count: usize) {
-    let mut last_idx = 0;
-    let mut total = 0;
-    for x in 0..count {
-        let idx = find_largest(v[last_idx..], count - x);
-        let joltage += v[idx] * 10_u64.pow((count - x - 1) as u64)
-        total += joltage;
-        last_idx = idx;
-    }
-}
-*/
-
-fn part1(battery_banks: impl AsRef<[Vec<u32>]>) {
-    let total: u32 = battery_banks
+fn turn_on_batteries(battery_banks: impl AsRef<[Vec<u32>]>, battery_count: usize) -> u64 {
+    battery_banks
         .as_ref()
         .iter()
         .map(|battery_bank| {
-            let (left, right) =
-                battery_bank
-                    .windows(2)
-                    .fold((0, 0), |(mut left, mut right), pair| {
-                        if pair.len() < 2 {
-                            if pair[0] > right {
-                                right = pair[0];
-                            }
-                        } else {
-                            if pair[0] > left {
-                                left = pair[0];
-                                right = pair[1];
-                            } else if pair[1] > right {
-                                right = pair[1];
-                            }
-                        }
-
-                        (left, right)
-                    });
-
-            left * 10 + right
-        })
-        .sum();
-
-    assert!(total == 17324);
-    println!("Total: {}", total);
-}
-
-// this probably could be generalized and re-used as part 1
-// if we used a vector instead of an array
-fn part2(battery_banks: impl AsRef<[Vec<u32>]>) {
-    const BATTERY_COUNT: usize = 12;
-
-    let total: u64 = battery_banks
-        .as_ref()
-        .iter()
-        .map(|battery_bank| {
-            let v = battery_bank.windows(BATTERY_COUNT).fold(
-                [0; BATTERY_COUNT],
+            let v = battery_bank.windows(battery_count).fold(
+                vec![0; battery_count],
                 |mut batteries, window| {
-                    let start = BATTERY_COUNT - window.len();
+                    let start = battery_count - window.len();
                     for idx in start..window.len() {
                         if window[idx] > batteries[idx] {
                             for j in idx..window.len() {
@@ -87,13 +21,24 @@ fn part2(battery_banks: impl AsRef<[Vec<u32>]>) {
             );
 
             let mut joltage = 0;
-            for x in 0..BATTERY_COUNT {
-                joltage += v[x] as u64 * 10_u64.pow((BATTERY_COUNT - x - 1) as u32);
+            for x in 0..battery_count {
+                joltage += v[x] as u64 * 10_u64.pow((battery_count - x - 1) as u32);
             }
 
             joltage
         })
-        .sum();
+        .sum()
+}
+
+fn part1(battery_banks: impl AsRef<[Vec<u32>]>) {
+    let total = turn_on_batteries(battery_banks, 2);
+
+    assert!(total == 17324);
+    println!("Total: {}", total);
+}
+
+fn part2(battery_banks: impl AsRef<[Vec<u32>]>) {
+    let total = turn_on_batteries(battery_banks, 12);
 
     assert!(total == 171846613143331);
     println!("Total: {}", total);
