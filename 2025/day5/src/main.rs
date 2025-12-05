06fd46e3873cbbe973c93b0cc40ce9a5c295461f
Shane Lillie <ignignokterr@gmail.com>
2025-12-05 07:11:55 -0800

    Part 1

--- 2025/day5/src/main.rs
+++ 2025/day5/src/main.rs
@@ -1,3 +1,37 @@
+use std::ops::RangeInclusive;
+
+fn part1(fresh_ids: impl AsRef<[RangeInclusive<usize>]>, available_ids: impl AsRef<[usize]>) {
+    let fresh_ids = fresh_ids.as_ref();
+    let available_ids = available_ids.as_ref();
+
+    let fresh_count = available_ids
+        .iter()
+        .filter(|available_id| fresh_ids.iter().any(|range| range.contains(available_id)))
+        .count();
+
+    assert!(fresh_count == 770);
+    println!("Fresh count: {}", fresh_count);
+}
+
 fn main() {
-    println!("Hello, world!");
+    let input = include_str!("../input.txt");
+
+    let (fresh_ids, available_ids) = input.trim().split_once("\n\n").unwrap();
+
+    let fresh_ids = fresh_ids
+        .lines()
+        .map(|line| {
+            let (start, end) = line.split_once('-').unwrap();
+            let start = start.parse::<usize>().unwrap();
+            let end = end.parse::<usize>().unwrap();
+            start..=end
+        })
+        .collect::<Vec<_>>();
+
+    let available_ids = available_ids
+        .lines()
+        .map(|line| line.parse::<usize>().unwrap())
+        .collect::<Vec<_>>();
+
+    part1(&fresh_ids, &available_ids);
 }
