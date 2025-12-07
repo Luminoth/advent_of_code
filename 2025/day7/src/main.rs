c413191ea87afd9b7d32e8eef07a48e1be4ee3fb
Shane Lillie <ignignokterr@gmail.com>
2025-12-07 09:46:11 -0800

    Part 2

--- 2025/day7/src/main.rs
+++ 2025/day7/src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::BTreeSet;
+use std::collections::{BTreeMap, BTreeSet};
 
 #[derive(Debug, Default, Copy, Clone, PartialEq, Eq, Hash, PartialOrd, Ord)]
 struct Vector2 {
@@ -58,8 +58,70 @@ fn part1(manifold: impl AsRef<[Vec<char>]>, start: Vector2) {
     println!("Splits: {}", splits);
 }
 
-fn part2(manifold: impl AsRef<[Vec<char>]>, _start: Vector2) {
-    let _manifold = manifold.as_ref();
+fn part2(manifold: impl AsRef<[Vec<char>]>, start: Vector2) {
+    let manifold = manifold.as_ref();
+
+    let mut beams = BTreeMap::new();
+    beams.insert(start, 1_usize);
+
+    let mut processing = Vec::new();
+
+    let mut completed = 0;
+    loop {
+        while let Some(beam) = beams.pop_first() {
+            processing.push(beam);
+        }
+
+        while let Some(mut beam) = processing.pop() {
+            beam.0.y += 1;
+            if beam.0.y >= manifold.len() {
+                completed += beam.1;
+                continue;
+            }
+
+            if manifold[beam.0.y][beam.0.x] == '^' {
+                //println!("split {} at ({:?})", beam.1, beam.0);
+                if beam.0.x > 0 {
+                    let mut left = beam;
+                    left.0.x -= 1;
+                    beams
+                        .entry(left.0)
+                        .and_modify(|c| {
+                            *c += left.1;
+                            //println!("overlap left: {}", *c);
+                        })
+                        .or_insert(left.1);
+                }
+
+                if beam.0.x < manifold[beam.0.y].len() - 1 {
+                    let mut right = beam;
+                    right.0.x += 1;
+                    beams
+                        .entry(right.0)
+                        .and_modify(|c| {
+                            *c += right.1;
+                            //println!("overlap right: {}", *c);
+                        })
+                        .or_insert(right.1);
+                }
+            } else {
+                beams
+                    .entry(beam.0)
+                    .and_modify(|c| {
+                        *c += beam.1;
+                        //println!("overlap straight: {}", *c);
+                    })
+                    .or_insert(beam.1);
+            }
+        }
+
+        if beams.is_empty() {
+            break;
+        }
+    }
+
+    assert!(completed == 13883459503480);
+    println!("Completed: {}", completed);
 }
 
 fn main() {
