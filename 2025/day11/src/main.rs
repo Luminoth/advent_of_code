76a7b8b5b23e0040a5c63f93248b1d45e64aa767
Shane Lillie <ignignokterr@gmail.com>
2025-12-12 22:03:45 -0800

    Part 1

--- 2025/day11/src/main.rs
+++ 2025/day11/src/main.rs
@@ -1,6 +1,36 @@
-use std::collections::{HashMap, HashSet};
+use std::collections::{BTreeSet, HashMap, HashSet};
 
-fn part1(_nodes: &HashMap<&str, HashSet<&str>>) {}
+fn check_path<'a>(
+    nodes: &HashMap<&'a str, BTreeSet<&'a str>>,
+    node: &'a str,
+    visited: &mut HashSet<&'a str>,
+) -> usize {
+    if node == "out" {
+        return 1;
+    }
+
+    if visited.contains(node) {
+        return 0;
+    }
+    visited.insert(node);
+
+    let mut paths = 0;
+
+    let connections = nodes.get(node).unwrap();
+    for connection in connections {
+        paths += check_path(nodes, connection, visited);
+    }
+
+    visited.remove(node);
+    paths
+}
+
+fn part1(nodes: &HashMap<&str, BTreeSet<&str>>) {
+    let paths = check_path(nodes, "you", &mut HashSet::new());
+
+    assert!(paths == 708);
+    println!("Paths: {}", paths);
+}
 
 fn main() {
     let input = include_str!("../input.txt");
@@ -9,7 +39,9 @@ fn main() {
         .lines()
         .map(|line| {
             let (node, connections) = line.split_once(":").unwrap();
-            let connections = connections.split_ascii_whitespace().collect::<HashSet<_>>();
+            let connections = connections
+                .split_ascii_whitespace()
+                .collect::<BTreeSet<_>>();
             (node.trim(), connections)
         })
         .collect::<HashMap<_, _>>();
