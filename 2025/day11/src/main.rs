62e12133dd43fae19823f8861e63ebfa0c12d058
Shane Lillie <ignignokterr@gmail.com>
2025-12-12 22:37:32 -0800

    Idk part 2

--- 2025/day11/src/main.rs
+++ 2025/day11/src/main.rs
@@ -1,34 +1,50 @@
-use std::collections::{BTreeSet, HashMap, HashSet};
+use std::collections::{BTreeSet, HashMap};
 
 fn check_path<'a>(
     nodes: &HashMap<&'a str, BTreeSet<&'a str>>,
     node: &'a str,
-    visited: &mut HashSet<&'a str>,
+    visited: &mut Vec<&'a str>, // was a HashSet in part 1 but part 2 may need to know the order?
+    required: &Vec<&str>,
 ) -> usize {
     if node == "out" {
+        for req in required {
+            if !visited.contains(req) {
+                return 0;
+            }
+        }
         return 1;
     }
 
-    if visited.contains(node) {
+    if visited.contains(&node) {
+        println!("cycle");
         return 0;
     }
-    visited.insert(node);
+    visited.push(node);
 
     let mut paths = 0;
 
     let connections = nodes.get(node).unwrap();
     for connection in connections {
-        paths += check_path(nodes, connection, visited);
+        let p = check_path(nodes, connection, visited, required);
+        //println!("{node} found: {p}");
+        paths += p;
     }
 
-    visited.remove(node);
+    visited.pop();
     paths
 }
 
 fn part1(nodes: &HashMap<&str, BTreeSet<&str>>) {
-    let paths = check_path(nodes, "you", &mut HashSet::new());
+    let paths = check_path(nodes, "you", &mut vec![], &vec![]);
 
     assert!(paths == 708);
+    println!("Paths: {}", paths);
+}
+
+fn part2(nodes: &HashMap<&str, BTreeSet<&str>>) {
+    let paths = check_path(nodes, "svr", &mut vec![], &vec!["dac", "fft"]);
+
+    //assert!(paths == ??);
     println!("Paths: {}", paths);
 }
 
@@ -47,4 +63,5 @@ fn main() {
         .collect::<HashMap<_, _>>();
 
     part1(&nodes);
+    part2(&nodes);
 }
